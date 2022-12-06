1e073af97fca0cc7dd8d725505317826c2d069cf
Professor Luciano <luciano.silva@sp.senac.br>
2022-12-06 15:05:58 -0300

    mudancas

--- filmes_tmdb/src/pages/Dashboard/index.js
+++ filmes_tmdb/src/pages/Dashboard/index.js
@@ -1,19 +1,65 @@
+import { useEffect, useState } from 'react'
 import {
     StyleSheet,
-    SafeAreaView,
+    TouchableOpacity,
     View,
-    Text
+    Text,
+    ScrollView
 } from 'react-native'
+import api from '../../services/api'
 
-export default function Dashboard(){
+export default function Dashboard() {
 
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>Dashboard</Text>
-            </View>
-        </SafeAreaView>
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+
+        async function loadFilmes() {
+            const response = await api.get('/movie/now_playing', {
+                params: {
+                    api_key: 'e3557a63a0916ff565660d0e9b496cba',
+                    language: 'pt-BR'
+                }
+            })
+            setFilmes(response.data.results)
+        }
+        loadFilmes()
+    }, [])
+    //console.log(filmes)
+
+
+    return (
+        <View style={styles.container}>
+            <Text>Dashboard</Text>
+            <ScrollView>
+                {filmes.map(filme => (
+                    <TouchableOpacity key={filme.id} style={styles.button}>
+                        <Text style={styles.titulos}>{filme.title}</Text>
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: '#4776F0',
+        fontWeight: 'bold',
+        marginTop: 10,
+        height: 30,
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+    titulos: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: 'bold'
+    }
+})
 
 
